859df77eab4e0fc1890e57f4df54a7b863cb9a9e
Aman Ibrahim <amanmibra@gmail.com>
2017-06-22 15:52:04 -0400

    Added user input

--- src/main.js
+++ src/main.js
@@ -1,27 +1,34 @@
 $(document).ready(function(){
-  $('#test').html("Testing...");
-  var key = "eoZA48hLZRnyiw9VJEl8o2Wg0UiyTICMTfKvahc0";
-  var lon = '-112.097796'; var lat = '36.098592';
-  var begin = formatDate(new Date("2015/05/22"));
-  var urlAddons = "lon=" + lon + "&lat=" + lat + "&begin=" + begin + "&api_key=DEMO_KEY";
+  $('#submit').click(function(){
+    $('#test').html("Testing...");
+    var key = "eoZA48hLZRnyiw9VJEl8o2Wg0UiyTICMTfKvahc0";
+    var lon = $('#lon').val();
+    var lat = $('#lat').val();
+    var begin = formatDate();
+    var urlAddons = "lon=" + lon + "&lat=" + lat + "&begin=" + begin + "&api_key=DEMO_KEY";
 
-  var url = "https://api.nasa.gov/planetary/earth/assets?" + urlAddons;
-  $.ajax({
-    url: url,
-    api_key: key,
-    lat: 36.098592,
-    lon: -112.097796,
-    begin: new Date("2017/05/22"),
-    success: function(data){
-      console.log('incoming data: ', data);
-    }
-  });
+    var url = "https://api.nasa.gov/planetary/earth/assets?" + urlAddons;
+    console.log(url);
+    $.ajax({
+      url: url,
+      api_key: key,
+      lat: 36.098592,
+      lon: -112.097796,
+      begin: new Date("2017/05/22"),
+      success: function(data){
+        console.log('incoming data: ', data);
+      }
+    });
+  })
+
+
+
+  function formatDate(){
+    var year = $('#year').val()
+    var month = $('#month').val();
+    var day = $('#day').val();
+
+    return year + '-' + month + '-' + day
+  }
+
 });
-
-function formatDate(date){
-  var year = date.getFullYear();
-  var month = date.getMonth() + 1;
-  var day = date.getDate();
-
-  return year + "-" + month + "-" + day;
-}
