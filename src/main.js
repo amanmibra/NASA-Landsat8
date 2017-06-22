1289dd288787633197066e45caa8d6d845bb13ef
Aman Ibrahim <amanmibra@gmail.com>
2017-06-22 16:18:01 -0400

    Added flyby method

--- src/main.js
+++ src/main.js
@@ -1,24 +1,27 @@
-$(document).ready(function(){
-  $('#submit').click(function(){
-    $('#test').html("Testing...");
-    var key = "eoZA48hLZRnyiw9VJEl8o2Wg0UiyTICMTfKvahc0";
-    var lon = $('#lon').val();
-    var lat = $('#lat').val();
-    var begin = "2013-02-11"; //Setting the begin date as the Landsat8 launch date
-    var urlAddons = "lon=" + lon + "&lat=" + lat + "&begin=" + begin + "&api_key=DEMO_KEY";
+const var myKey = "eoZA48hLZRnyiw9VJEl8o2Wg0UiyTICMTfKvahc0";
+const var url = "https://api.nasa.gov/planetary/earth/assets?"
+const var begin = "2013-02-11"; //Setting the begin date as the Landsat8 launch date
 
-    var url = "https://api.nasa.gov/planetary/earth/assets?" + urlAddons;
-    console.log(url);
+$(document).ready(function() {
+  $('#submit').click(function() {
+    flyby($('#lon').val(), $('#lat').val());
+  })
+
+  void function flyby(double long, double lat){
+    var urlAddons = "lon=" + long + "&lat=" + lat + "&begin=" + begin + "&api_key=DEMO_KEY";
+    var newURL = url + urlAddons;
+    console.log(newURL);
+
     $.ajax({
       url: url,
-      success: function(data){
+      success: function(data) {
         console.log('incoming data: ', data);
-        if("error" in data){
+        if ("error" in data) {
           console.log("here")
           $('#error-mess').html("Error: " + data.error.toString());
         }
       }
     });
-  })
+  }
 
 });
