53f07555c925843d48054fdc463e9105ed536bca
Aman Ibrahim <amanmibra@gmail.com>
2017-06-22 16:05:38 -0400

    Added error checks

--- src/main.js
+++ src/main.js
@@ -4,31 +4,21 @@ $(document).ready(function(){
     var key = "eoZA48hLZRnyiw9VJEl8o2Wg0UiyTICMTfKvahc0";
     var lon = $('#lon').val();
     var lat = $('#lat').val();
-    var begin = formatDate();
+    var begin = "2013-02-11"; //Setting the begin date as the Landsat8 launch date
     var urlAddons = "lon=" + lon + "&lat=" + lat + "&begin=" + begin + "&api_key=DEMO_KEY";
 
     var url = "https://api.nasa.gov/planetary/earth/assets?" + urlAddons;
     console.log(url);
     $.ajax({
       url: url,
-      api_key: key,
-      lat: 36.098592,
-      lon: -112.097796,
-      begin: new Date("2017/05/22"),
       success: function(data){
         console.log('incoming data: ', data);
+        if("error" in data){
+          console.log("here")
+          $('#error-mess').html("Error: " + data.error.toString());
+        }
       }
     });
   })
-
-
-
-  function formatDate(){
-    var year = $('#year').val()
-    var month = $('#month').val();
-    var day = $('#day').val();
-
-    return year + '-' + month + '-' + day
-  }
 
 });
